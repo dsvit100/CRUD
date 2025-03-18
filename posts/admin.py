ec9d3a354378b644461f2a4f9591953fd2c5fd3c
hyojin shin <dsvit100hjin@gmail.com>
2025-03-18 11:45:20 +0900

    admin에 모델 등록

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+# 현재 내 위치에 있는 model 파일의 post 클래스 불러오기
+from .models import Post
 
 # Register your models here.
+
+# 위에 불러온 class Post를 관리자페이지에 register(등록)하기
+admin.site.register(Post)
